415d3d80e4944de671253a91adab86a2dc629565
Thanh Tien Nguyen <thanhtien.nguyen@vonaffenfels.de>
2020-08-16 16:50:48 +0200

    add icons

--- src/js/workfiles.js
+++ src/js/workfiles.js
@@ -3,4 +3,31 @@ jQuery(document).ready(function() {
       function() {
         $( ".select" ).toggle( "active" );
       }
+
+    jQuery(".donate").click(function() {
+        jQuery(".donate-form__content").toggleClass("show");
+        jQuery(this).toggleClass("donate__content--active");
+    });
+
+    jQuery(".join").click(function() {
+        jQuery(".donate-text__content").toggleClass("show");
+        jQuery(this).toggleClass("donate__content--active");
+    });
+
+    jQuery(".join").click(function() {
+        jQuery(".donate-text__content").addClass("hide");
+        jQuery(".donate-text__content").removeClass("show");
+        jQuery(".donate").removeClass("hide");
+    });
+
+    jQuery(".donate").click(function() {
+        jQuery(".donate-form__content").addClass("hide");
+        jQuery(".donate-form__content").removeClass("show");
+        jQuery(".join").removeClass("hide");
+    });
+
+    jQuery("slider__item").click(function() {
+        jQuery(".container--project").addClass("show");
+    });
 });
+
