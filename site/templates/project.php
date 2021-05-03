42939e02acfe5dec56d53f22ba752d317e64cfa4
Samuel Kehl <samuel.kehl@vinati.de>
2021-05-03 20:46:15 +0200

    move tags to bottom, fix project-page layout

--- site/templates/project.php
+++ site/templates/project.php
@@ -10,18 +10,19 @@
           <div class="col col--12 section-headline">
             <p><?= $page->title() ?></p>
           </div>
-          <div class="col col--12 section-meta">
-            <time class="project-date"><?= $page->date()->toDate('d F Y') ?></time>
-            <?php if ($page->tags()->isNotEmpty()) : ?>
-              <p class="project-tags tags"><?= $page->tags() ?></p>
-            <?php endif ?>
-          </div>
           <div class="col col--12 project-inline-container">
             <div class="col col--12 project-text-container">
               <div class="project-text text">
                 <?= $page->text()->kirbytext() ?>
               </div>
             </div>
+          </div>
+          <div class="col col--12 section-meta">
+            <?php if ($page->tags()->isNotEmpty()) : ?>
+              <p class="project-tags tags"><time class="project-date"><?= $page->date()->toDate('d F Y') ?></time> | <?= $page->tags() ?></p>
+            <?php else: ?>
+              <time class="project-date"><?= $page->date()->toDate('d F Y') ?></time>
+            <?php endif ?>
           </div>
         </div>
       </div>      
